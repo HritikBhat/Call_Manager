e3fd4c0abc7e103eb69892ae65f8369f89b4943c
HritikBhat <bhathritik399@gmail.com>
2020-01-09 23:33:31 +0530

    Auto Height and Width measuring buttons on main_activity!!

--- app/src/main/java/com/hritik/callmanager/MainActivity.java
+++ app/src/main/java/com/hritik/callmanager/MainActivity.java
@@ -6,13 +6,11 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Color;
-import android.graphics.Typeface;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
-import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
